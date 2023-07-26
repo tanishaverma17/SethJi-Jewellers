602e0ed965bbbc2b482646261c119a13baafb629
tanishaverma17 <76125836+tanishaverma17@users.noreply.github.com>
2023-07-26 21:02:00 +0530

    GUI added
    html, css done

---
+++ script.js
@@ -0,0 +1,8 @@
+const bar = document.querySelector("#bar");
+const nav = document.querySelector("#navbar");
+if(bar)[    
+    bar.addEventListener("click",function clickEventHandler(){
+        // console.log("click");
+        nav.classList.add('active');
+    })
+]
